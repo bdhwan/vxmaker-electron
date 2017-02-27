c7520739a37d17115874ca0d89c1ef273291c3d7
leeermk <zyrkwhdk@gmail.com>
2017-02-27 18:12:22 +0900

    Init layout (#3)
    * init-menu layout
    
    * 메인화면 레이아웃
    
    * init 리스트 정렬, 호버효과
    
    * -list 보이다 안보이다
    -hover효과
    -레이아웃 정돈
    
    * input 마무리
    
    input 정리,project folder 경로 바꾸기

--- src/app/init/init.component.ts
+++ src/app/init/init.component.ts
@@ -19,8 +19,9 @@ export class InitComponent implements OnInit {
 
   ngOnInit() {
 
-    electron.ipcRenderer.sendSync('change-window', 800, 600, true);
+    electron.ipcRenderer.sendSync('change-window', 800, 502, true);
     // console.log() 
   }
+
 
 }
