894a6b7ac2d0b3e1c58931e846b57ed33d65fc25
bdhwan <bdhwan@gmail.com>
2017-03-14 13:24:08 +0900

    화면 구성중
    화면 구성중

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ElementRef, ViewChild } from '@angular/core';
 
 
 
@@ -9,11 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviewComponent implements OnInit {
 
-   simpleDrop: any = null;
+  @ViewChild('mainScreen') elementView: ElementRef;
+
 
   constructor() { }
 
   ngOnInit() {
+
+    console.log(" this.viewHeight = " + this.elementView.nativeElement.offsetHeight);
+
   }
+
+
+  onResize(event) {
+
+    console.log("event.target.innerWidth =" + event.target.innerWidth + ", event.target.innerHeight=" + event.target.innerHeight);
+    console.log(" this.viewHeight = " + this.elementView.nativeElement.offsetHeight);
+
+  }
+
+
+
+  public setObjectData(data): void {
+    
+  }
+
+
 
 }
