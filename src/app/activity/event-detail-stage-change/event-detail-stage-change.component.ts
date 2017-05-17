b7e5096946e29fba77188c45aafdd88105ff5ed4
leeermk <minkyung327@naver.com>
2017-05-17 17:13:01 +0900

    인터폴레이터 추가
    -인터폴레이터 추가
    -스크롤바 안보이게

--- src/app/activity/event-detail-stage-change/event-detail-stage-change.component.ts
+++ src/app/activity/event-detail-stage-change/event-detail-stage-change.component.ts
@@ -19,21 +19,126 @@ export class EventDetailStageChangeComponent implements OnInit {
 
   presetInterpolatorList = [
     {
-      'name': 'easyInOut',
-      'value': [0, 0, 0.5, 1]
+      'name': 'linear',
+      'value': [0, 0, 1, 1]
     },
     {
-      'name': 'easyInOut1',
-      'value': [0, 0, 0.6, 1]
+      'name': 'ease',
+      'value': [0.25, 0.1, 0.25, 1]
+    },
+//ease-in
+    {
+      'name': 'easeIn',
+      'value': [0.42, 0, 1, 1]
+    },
+      {
+      'name': 'easeInQuad',
+      'value': [0.55, 0.085, 0.68, 0.53]
+    },
+      {
+      'name': 'easeInCubic',
+      'value': [0.55, 0.055, 0.675, 0.19]
+    },
+      {
+      'name': 'easeInQuart',
+      'value': [0.895, 0.03, 0.685, 0.22]
+    },
+      {
+      'name': 'easeInQuint',
+      'value': [0.755, 0.05, 0.855, 0.06]
+    },
+      {
+      'name': 'easeInSine',
+      'value': [0.47, 0, 0.745, 0.715]
+    },
+      {
+      'name': 'easeInExpo',
+      'value': [0.95, 0.05, 0.795, 0.035]
+    },
+      {
+      'name': 'easeInCirc',
+      'value': [0.6, 0.04, 0.98, 0.335]
+    },
+      {
+      'name': 'easeInBack',
+      'value': [0.6, -0.28, 0.735, 0.045]
+    },
+//ease-out
+
+  {
+      'name': 'easeOut',
+      'value': [0, 0, 0.58, 1]
     },
     {
-      'name': 'easyInOut2',
-      'value': [0, 0, 0.7, 1]
+      'name': 'easeOutQuad',
+      'value': [0.25, 0.46, 0.45, 0.94]
     },
-    {
-      'name': 'easyInOut3',
-      'value': [0, 0, 0.8, 1]
+     {
+      'name': 'easeOutCubic',
+      'value': [0.215, 0.61, 0.355, 1]
+    },
+     {
+      'name': 'easeOutQuart',
+      'value': [0.165, 0.84, 0.44, 1]
+    },
+     {
+      'name': 'easeOutQuint',
+      'value': [0.23, 1, 0.32, 1]
+    },
+     {
+      'name': 'easeOutSine',
+      'value': [0.39, 0.575, 0.565, 1]
+    },
+     {
+      'name': 'easeOutExpo',
+      'value': [0.19, 1, 0.22, 1]
+    },
+     {
+      'name': 'easeOutCirc',
+      'value': [0.075, 0.82, 0.165, 1]
+    },
+     {
+      'name': 'easeOutBack',
+      'value': [0.175, 0.885, 0.32, 1.275]
+    },
+       //ease-in-out
+        {
+      'name': 'easeInOut',
+      'value': [0.42, 0, 0.58, 1]
+    },
+     {
+      'name': 'easeInOutQuad',
+      'value': [0.455, 0.03, 0.515, 0.955]
+    },
+     {
+      'name': 'easeInOutCubic',
+      'value': [0.645, 0.045, 0.355, 1]
+    },
+     {
+      'name': 'easeInOutQuart',
+      'value': [0.77, 0, 0.175, 1]
+    },
+     {
+      'name': 'easeInOutQuint',
+      'value': [0.86, 0, 0.07, 1]
+    },
+     {
+      'name': 'easeInOutSine',
+      'value': [0.445, 0.05, 0.55, 0.95]
+    },
+     {
+      'name': 'easeInOutExpo',
+      'value': [1, 0, 0, 1]
+    },
+     {
+      'name': 'easeInOutCirc',
+      'value': [0.785, 0.135, 0.15, 0.86]
+    },
+     {
+      'name': 'easeInOutBack',
+      'value': [0.68, -0.55, 0.265, 1.55]
     }
+    
   ];
 
 
