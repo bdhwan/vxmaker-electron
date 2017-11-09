024d0465b029685a29999e1175d9561fa87295ad
hersheysaid <hersheysaid@gmail.com>
2017-11-09 09:16:52 +0900

    rg

--- src/app/activity/event-list-cell/event-list-cell.component.spec.ts
+++ src/app/activity/event-list-cell/event-list-cell.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { EventListCellComponent } from './event-list-cell.component';
 
 describe('EventListCellComponent', () => {
